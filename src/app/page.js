5fdd3c00e17fbb1fef29268701bb629dab1636c7
farhan0184 <shahariar0184@gmail.com>
2023-08-21 11:34:24 +0600

    navber first-commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,30 @@
+import Navbar from '@/components/navbar'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <div>
-      <h1 className='text-6xl'>this is home</h1>
+      <Navbar />
+      <div className=''>
+        <div className='bg-slate-400 h-[200vh]'>
+
+        </div>
+        <div className='bg-slate-500 h-[200vh]'>
+
+        </div>
+        <div className='bg-slate-600 h-[200vh]'>
+
+        </div>
+        <div className='bg-slate-700 h-[200vh]'>
+
+        </div>
+        <div className='bg-slate-800 h-[200vh]'>
+
+        </div>
+        <div className='bg-slate-900 h-[200vh]'>
+
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,30 @@
-export default function Navbar(){
-    return(
-        <div>
-            <h1>Navbar</h1>
-        </div>
+import Image from "next/image";
+import Logo from '/public/assets/images/logo.png'
+import Link from "next/link";
+
+export default function Navbar() {
+    return (
+        <header className="bg-[#ffc017] border-b-[1px] border-black fixed top-0 left-0 w-full transition-[0.6s]">
+            <div className="flex py-4 w-[90%] 2xl:w-[60%] mx-auto  ">
+                <div className="flex items-center w-[40%]">
+                    <Image
+                        src={Logo}
+                        alt="Logo"
+                        className="sm:w-[60%] w-full"
+                    />
+                </div>
+                <div className="flex  justify-end items-center text-[14px] w-full">
+                    <ul className="hidden md:flex gap-5 ">
+                        <li><Link href={'#'}>Our story</Link></li>
+                        <li><Link href={'#'}>Membership</Link></li>
+                        <li><Link href={'#'}>Write</Link></li>
+                    </ul>
+                    <div className="flex">
+                        <button className="hidden xsm:block py-2 mx-5">Sign In</button>
+                        <button className="text-white bg-black py-2 px-3 rounded-[30px]">Get started</button>
+                    </div>
+                </div>
+            </div>
+        </header>
     )
 }
