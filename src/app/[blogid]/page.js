619e3864eb666e35b983ec9bc6f50d595a18e9e4
farhan0184 <shahariar0184@gmail.com>
2023-08-24 13:11:28 +0600

    person another blog with component blog

--- src/app/[blogid]/page.js
+++ src/app/[blogid]/page.js
@@ -8,6 +8,7 @@ import Image from "next/image"
 import { BsFillStarFill } from 'react-icons/bs'
 import { MdOutlineBookmarkAdd } from 'react-icons/md'
 import { BsFillEnvelopePlusFill } from 'react-icons/bs'
+import Blog from "@/components/blog"
 
 export default function Page({ params }) {
     const blog = blogs.find(item => item.id == params.blogid)
@@ -109,40 +110,7 @@ export default function Page({ params }) {
                     <div className="grid sm:grid-cols-2 grid-cols-1 gap-10">
                         {
                             blogs.filter(item => item.person === person).map(item =>
-                                <div key={item.id} className="">
-                                    <div>
-                                        <Image
-                                            src={item.banner}
-                                            alt={item.person}
-                                            className="h-[200px]"
-                                        />
-                                    </div>
-                                    <div className="my-3 relative h-[240px]">
-                                        <div>
-                                            <div className="flex gap-2 items-center">
-                                                <div className="bg-slate-300 w-7 h-7 flex items-center justify-center rounded-[30px]"><Image src={item.img} alt={item.person} className="w-5 h-5 rounded-[30px]" /></div>
-                                                <p className="text-[14px]">{item.person} <span className=""> in </span> {item.from}</p>
-                                            </div>
-                                            <div className="my-4">
-                                                <h1 className="text-2xl font-bold mb-2">{item.title}</h1>
-                                                <p className="text-[16px] text-[#b3afaf] text-justify">{item.details.slice(0, 80)}...</p>
-                                            </div>
-                                        </div>
-                                        <div className="absolute bottom-0 w-full">
-                                            <div className="text-[12px] my-3 flex items-center">
-                                                <BsFillStarFill className='text-[#ffc017]' />
-                                                <p className="text-[#c4c0c0]"> . {time} . {date}</p>
-                                            </div>
-                                            <div className="flex justify-between items-center">
-                                                <div className="flex gap-4 ">
-                                                    <LikeComment isLike={true} item={item.like} />
-                                                    <LikeComment isLike={false} item={item.comment} />
-                                                </div>
-                                                <MdOutlineBookmarkAdd className='text-2xl' />
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                                <Blog key={item.id} item={item}/>
                             )
                         }
                     </div>
