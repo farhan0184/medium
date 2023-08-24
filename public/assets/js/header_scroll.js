7b11b2aa7093804a2748052a3ca4251b2016bb4a
farhan0184 <shahariar0184@gmail.com>
2023-08-24 14:19:06 +0600

    delete js folder

--- public/assets/js/header_scroll.js
+++
@@ -1,9 +0,0 @@
-console.log('helo wold')
-function scrollHeader(){
-    window.addEventListener('scroll',function(){
-        let header = document.querySelector('header');
-        header.classList.toggle('sticky', window.screenY > 0)
-    })
-}
-
-scrollHeader()
